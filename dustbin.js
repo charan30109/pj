b92e9c862fe2169b319ee9caa6fe605f21add540
charan30109 <72085935+charan30109@users.noreply.github.com>
2020-10-30 16:11:54 -0700

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,38 @@
+class dustbin {
+    constructor(x,y,width,height) {
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image = loadImage("dustbingreen.png");
+      }
+      display(){
+        push();
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill("red");
+        rect(pos.x, pos.y, this.width, this.height);
+        imageMode(CENTER);
+        image(this.image, this.body.position.x, this.body.position.y, this.width, 200);
+        pop();
+        
+      }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
